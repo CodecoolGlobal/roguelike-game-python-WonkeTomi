adbc24a53606250aa2079bd7ac8b8adb414d8ab9
MiMi0001 <willijiem@gmail.com>
2022-06-29 22:12:48 +0200

    Sound test 3

--- sounds.py
+++ sounds.py
@@ -20,7 +20,7 @@ def load_sound(sound_file_name):
 
 def playsound_background_music():
     init_music_player()
-    sound1 = pygame.mixer.Sound("Willow_Escape.wav")
+    sound1 = pygame.mixer.Sound("Willow_Escape_2.wav")
     pygame.mixer.find_channel().play(sound1)
 
 
