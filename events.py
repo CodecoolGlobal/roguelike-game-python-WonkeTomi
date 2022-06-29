a512895f327b6845a915f3f43b03df57ffff1385
SzimBensze <bence.smile.he@gmail.com>
2022-06-29 11:54:23 +0200

    Added functions skeleton

--- events.py
+++ events.py
@@ -1,2 +1,30 @@
 import characters
 import engine
+
+
+def check_event(item_type):
+    pass
+
+
+def check_item(item):
+    pass
+
+
+def event_item(item, message):
+    pass
+
+
+def event_fight(character, monster):
+    pass
+
+
+def event_special(spec, decisions):
+    pass
+
+
+def event_win(character, message):
+    pass
+
+
+def event_die(character, message):
+    pass
