11fe48f66f21c29cafc642666d39ae52d7023b5f
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2022-06-30 15:23:33 +0200

    story for the randomevents

--- special_events.py
+++ special_events.py
@@ -3,14 +3,13 @@ import random
 
 GET_CHOICE = ["Yes", "No"]
 
-FAKTOR = ["You are lucky, ", "You get fucked, "]
+FAKTOR = ["You are lucky!", "You get fucked!"]
 OUTCOME = ["You lost 20HP", "You got 20HP"]
 
-POSSIBILITIES = {':evergreen_tree:': ["This peach is rotten inside and infested with worms.", "It was really tasty."]}
-EAT_PEACH = "You have just found a shiny, juicy lookin thicc peach. Do you taste it or not?"
-SAVING = "Would you like to save?"
-CASTLE = "You have found an empty looking castle and though what could be inside. Would you like to look around?"
-LAMA = "You have run into a cute, handsome and kind lama and thinking of petting the cutie. Do you want to pet it or not"
+EAT_PEACH = "You have just found a shiny, juicy lookin thicc peach."
+SAVING = "You find a save icon."
+CASTLE = "You have found an empty looking castle and though what could be inside. So you went inside."
+LAMA = "You have run into a cute, handsome and kind looking lama and thinking of petting the cutie."
 BANANA = "You have found a banana. Do you take it or not?"
 ROLL_DIE = "You have found a magical dice. Do you wanna roll it or not? (Might something happen)"
 ONION = "You have found an onion apparently there are other ingredients for a stew. Do you want to cook it or not?"
@@ -19,6 +18,12 @@ ICE_CREAM = "You have found an ice cream and it looks like it's melting. Do you 
 MIDDLE_FINGER1 = "You have just found a jerk with it's middle finger up and kicked his ass. Do you take +10 attack or did it just for fun?"
 MIDDLE_FINGER2 = "You have just found a jerk with it's middle finger up and he kicked your ass. It hits you with 50 damage you weak nerd."
 MUSHROOM = "You have found a death cap mushroom and you are hungry. Do you eat it or not?"
+POSSIBILITIES = {
+    ':evergreen_tree:': ["This peach is rotten inside and infested with worms.", "It was really tasty.", EAT_PEACH],
+    ':floppy_disk:': ['There is no saving in this game.', 'There is no saving in this game but we ar kind.', SAVING],
+    ':castle:': ['There are some traps left in the castle and you walk into one.', 'You find some food and ate it.', CASTLE],
+    ':llama:': ["The lama wasn't so happy about it so it split at you.", 'The lama was really kind and give his flesh to you what you can eat.', LAMA]
+    }
 
 events = [POSSIBILITIES, ]
 
@@ -31,4 +36,5 @@ def choose_special(special):
             print(OUTCOME[outcome])
             
         
-        
+if __name__ == '__main__':
+    choose_special([[''], [''], ['', '', ':evergreen_tree:']])
