ded1b959bd3df2e1bebb499ca499a7161a472813
SzimBensze <bence.smile.he@gmail.com>
2022-06-30 11:13:12 +0200

    Init fight file

---
+++ fight.py
@@ -0,0 +1,18 @@
+import characters
+import events
+
+
+def attack_phase():
+    pass
+
+
+def use_buttons():
+    pass
+
+
+def calc_damage(character, enemy, fight_type):
+    pass
+
+
+def use_inventory(character):
+    pass
