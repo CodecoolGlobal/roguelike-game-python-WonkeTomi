29c06309621c18f4225ff2e69721a032b18ea76c
Poga <pozsonyigabor96@gmail.com>
2022-06-30 15:13:57 +0200

    Added choosing section for special event

--- special_events.py
+++ special_events.py
@@ -1,12 +1,11 @@
 import ui
 import random
+import characters
 
-GET_CHOICE = ["Yes", "No"]
 
-FAKTOR = ["You are lucky, ", "You get fucked, "]
-OUTCOME = ["You lost 20HP", "You got 20HP"]
+FACTOR = ["You get fucked!", "You are lucky!"]
+OUTCOME = ["You lost 20HP.", "You got 20HP."]
 
-POSSIBILITIES = {':evergreen_tree:': ["This peach is rotten inside and infested with worms.", "It was really tasty."]}
 EAT_PEACH = "You have just found a shiny, juicy lookin thicc peach. Do you taste it or not?"
 SAVING = "Would you like to save?"
 CASTLE = "You have found an empty looking castle and though what could be inside. Would you like to look around?"
@@ -19,16 +18,25 @@ ICE_CREAM = "You have found an ice cream and it looks like it's melting. Do you 
 MIDDLE_FINGER1 = "You have just found a jerk with it's middle finger up and kicked his ass. Do you take +10 attack or did it just for fun?"
 MIDDLE_FINGER2 = "You have just found a jerk with it's middle finger up and he kicked your ass. It hits you with 50 damage you weak nerd."
 MUSHROOM = "You have found a death cap mushroom and you are hungry. Do you eat it or not?"
+POSSIBILITIES = {':evergreen_tree:': ["This peach is rotten inside and infested with worms.", "It was really tasty.", EAT_PEACH]
+}
 
-events = [POSSIBILITIES, ]
 
 def choose_special(special):
     outcome = random.randint(0, 1)
-    for event in events:
-        if special[2][2] == event.keys():
-            print(FAKTOR[outcome])
-            print(event[special[2][2]][outcome])
-            print(OUTCOME[outcome])
-            
-        
-        
+    event_index = 2
+    on_goings = []
+    for event in POSSIBILITIES:
+        if special[2][2] in event.keys():
+            on_goings.append(event[special[2][2]][event_index])
+            on_goings.append(FACTOR[outcome])
+            on_goings.append(event[special[2][2]][outcome])
+            on_goings.append(OUTCOME[outcome])
+            if outcome == 0:
+                characters.main_character["HP"] -= 20
+            else:
+                characters.main_character["HP"] += 20
+
+
+if __name__ == "__main__":
+    choose_special([[''], [''], ["", "", ':evergreen_tree:']])    
